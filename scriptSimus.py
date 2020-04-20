e2b62ed71aa9352f655c495f5448ec6d46814af5
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2020-04-21 01:48:21 +0200

    Cosmetic changes

--- scriptSimus.py
+++ scriptSimus.py
@@ -1,72 +1,105 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fixed Parameters
-nbQuestionsPerStudent = 20
-nbStudents = 500
-nbStudentsPerGroup = 10
+#########################################
+#                                       #
+# Simulations and Distances computation #
+#                                       #
+#########################################
 
-nbQuestions = np.arange(nbQuestionsPerStudent+1,nbStudents*nbQuestionsPerStudent,5)
+def mainSimulation(params):
+    (nbQuestionsPerStudent,nbStudents,nbStudentsPerGroup)=params
+    nbQuestions = np.arange(nbQuestionsPerStudent+1,nbStudents*nbQuestionsPerStudent,5)
 
-meanPWDistances=np.zeros((len(nbQuestions),))
-minmaxPWDistances=np.zeros((len(nbQuestions),2))
-meanDistances=np.zeros((len(nbQuestions),))
-minmaxDistances=np.zeros((len(nbQuestions),2))
-cpt=0
-for n in nbQuestions:
-    # Generate nbQuestions random partitions of the set {0,n}
-    randperm = np.zeros((nbQuestionsPerStudent,nbStudentsPerGroup))
-    for i in range(nbStudentsPerGroup):
-        randperm[:,i]=np.random.permutation(n)[0:nbQuestionsPerStudent]
-    # Compute all pairwise distances between partitions (edit distance)
-    distance=[]
-    unionOfIntersect=[]
-    for i in range(nbStudentsPerGroup):
-        for j in range(nbStudentsPerGroup):
-            if i<j:
-                unionOfIntersect.append(np.intersect1d(randperm[:,i],randperm[:,j]))
-                dist=len(np.intersect1d(randperm[:,i],randperm[:,j]))
-                distance.append(dist)
-    meanPWDistances[cpt]=np.mean(np.array(distance))
-    minmaxPWDistances[cpt,0]=np.min(np.array(distance))
-    minmaxPWDistances[cpt,1]=np.max(np.array(distance))
-    # Union of all questions => diff between individual questions and union
-    union=np.unique(randperm.flatten())
-    distance=[]
-    unionOfIntersect=np.unique(np.concatenate(unionOfIntersect))
-    for i in range(nbStudentsPerGroup):
-        dist=len(np.setdiff1d(randperm[:,i],unionOfIntersect))
-        distance.append(dist)
-    meanDistances[cpt]=np.mean(np.array(distance))
-    minmaxDistances[cpt,0]=np.min(np.array(distance))
-    minmaxDistances[cpt,1]=np.max(np.array(distance))
-    cpt=cpt+1
-    print(n)
+    meanPWDistances=np.zeros((len(nbQuestions),))
+    minmaxPWDistances=np.zeros((len(nbQuestions),2))
+    meanDistances=np.zeros((len(nbQuestions),))
+    minmaxDistances=np.zeros((len(nbQuestions),2))
+    cpt=0
+    for n in nbQuestions:
 
+        # Generate nbQuestions random partitions of the set {0,n}
+        randperm = np.zeros((nbQuestionsPerStudent,nbStudentsPerGroup))
+        for i in range(nbStudentsPerGroup):
+            randperm[:,i]=np.random.permutation(n)[0:nbQuestionsPerStudent]
 
-plt.figure(figsize=(12,8))
-plt.semilogx(nbQuestions,meanPWDistances,'k-')
-plt.fill_between(nbQuestions,minmaxPWDistances[:,0],minmaxPWDistances[:,1])
-plt.xlabel('Number of all the questions required for the exam')
-plt.ylabel('Questions')
-plt.legend(['Average','Min-Max for '+str(nbStudentsPerGroup)+' students per group'])
-plt.title('Shared questions between two exam subjects with ' + str(nbQuestionsPerStudent) + ' questions')
+        # Compute all pairwise distances between partitions (edit distance)
+        distance=[]
+        unionOfIntersect=[]
+        for i in range(nbStudentsPerGroup):
+            for j in range(nbStudentsPerGroup):
+                if i<j:
+                    unionOfIntersect.append(np.intersect1d(randperm[:,i],randperm[:,j]))
+                    dist=len(np.intersect1d(randperm[:,i],randperm[:,j]))
+                    distance.append(dist)
+        meanPWDistances[cpt]=np.mean(np.array(distance))
+        minmaxPWDistances[cpt,0]=np.min(np.array(distance))
+        minmaxPWDistances[cpt,1]=np.max(np.array(distance))
 
-plt.savefig("Figure1.png")
+        # Union of all questions => diff between individual questions and union
+        union=np.unique(randperm.flatten())
+        distance=[]
+        unionOfIntersect=np.unique(np.concatenate(unionOfIntersect))
+        for i in range(nbStudentsPerGroup):
+            dist=len(np.setdiff1d(randperm[:,i],unionOfIntersect))
+            distance.append(dist)
+        meanDistances[cpt]=np.mean(np.array(distance))
+        minmaxDistances[cpt,0]=np.min(np.array(distance))
+        minmaxDistances[cpt,1]=np.max(np.array(distance))
+        cpt=cpt+1
+    return (meanPWDistances,minmaxPWDistances,meanDistances,minmaxDistances,nbQuestions)
 
-plt.figure(figsize=(12,8))
-plt.semilogx(nbQuestions,nbQuestionsPerStudent-meanDistances,'k-')
-plt.fill_between(nbQuestions,nbQuestionsPerStudent-minmaxDistances[:,0],nbQuestionsPerStudent-minmaxDistances[:,1])
-plt.xlabel('Number of all the questions required for the exam')
-plt.ylabel('Questions' )
-plt.legend(['Average','Min-Max for '+str(nbStudentsPerGroup)+' students per group'])
-plt.title('Questions among ' + str(nbQuestionsPerStudent) + ' shared in a group of '+str(nbStudentsPerGroup)+' students')
+#################
+#               #
+# Visualization #
+#               #
+#################
 
-plt.savefig("Figure2.png")
+def vizu(nbQuestions,meanPWDistances,minmaxPWDistances,meanDistances,minmaxDistances,params):
+    (nbQuestionsPerStudent,nbStudents,nbStudentsPerGroup)=params
 
-plt.figure(figsize=(12,8))
-plt.semilogx(nbQuestions,meanPWDistances,'r-')
-plt.semilogx(nbQuestions,nbQuestionsPerStudent-meanDistances,'b-')
-plt.legend(['Shared questions between two exam subjects','Questions shared in a group of '+str(nbStudentsPerGroup)+' students'])
+    plt.figure(figsize=(12,8))
+    plt.semilogx(nbQuestions,meanPWDistances,'k-')
+    plt.fill_between(nbQuestions,minmaxPWDistances[:,0],minmaxPWDistances[:,1])
+    plt.xlabel('Number of all the questions required for the exam')
+    plt.ylabel('Questions')
+    plt.legend(['Average','Min-Max for '+str(nbStudentsPerGroup)+' students per group'])
+    plt.title('Shared questions between two exam subjects with ' + str(nbQuestionsPerStudent) + ' questions')
+    plt.savefig("Figure1.png")
+    plt.show()
 
-plt.savefig("Figure3.png")
+    plt.figure(figsize=(12,8))
+    plt.semilogx(nbQuestions,nbQuestionsPerStudent-meanDistances,'k-')
+    plt.fill_between(nbQuestions,nbQuestionsPerStudent-minmaxDistances[:,0],nbQuestionsPerStudent-minmaxDistances[:,1])
+    plt.xlabel('Number of all the questions required for the exam')
+    plt.ylabel('Questions' )
+    plt.legend(['Average','Min-Max for '+str(nbStudentsPerGroup)+' students per group'])
+    plt.title('Questions among ' + str(nbQuestionsPerStudent) + ' shared in a group of '+str(nbStudentsPerGroup)+' students')
+    plt.savefig("Figure2.png")
+    plt.show()
+
+    plt.figure(figsize=(12,8))
+    plt.semilogx(nbQuestions,meanPWDistances,'r-')
+    plt.semilogx(nbQuestions,nbQuestionsPerStudent-meanDistances,'b-')
+    plt.legend(['Shared questions between two exam subjects','Questions shared in a group of '+str(nbStudentsPerGroup)+' students'])
+    plt.savefig("Figure3.png")
+    plt.show()
+
+########
+#      #
+# Main #
+#      #
+########
+
+if __name__ == '__main__':
+    print('Number of students: \n')
+    nbStudents=input()
+    print('\n Number of questions: \n')
+    nbQuestionsPerStudent=input()
+    print('\n Number of students per group')
+    nbStudentsPerGroup=input()
+    params=(int(nbQuestionsPerStudent),int(nbStudents),int(nbStudentsPerGroup))
+
+    (meanPWDistances,minmaxPWDistances,meanDistances,minmaxDistances,nbQuestions)=mainSimulation(params)
+    vizu(nbQuestions,meanPWDistances,minmaxPWDistances,meanDistances,minmaxDistances,params)
+
